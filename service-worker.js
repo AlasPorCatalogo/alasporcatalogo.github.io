40d6afd01a1c2501abcb6b21f5d0b7fc80026ae7
AlasPorCatalogo <alasporcatalogo@gmail.com>
2026-09-13 02:17:46 -0700

    7

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 //This code is combined from the same google example And from a mozilla example:
 //https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers#deleting_old_caches
-const cacheName = 'cache-v3.0.1.11';
-const precacheResources = ['/', 'index.html', 'Fechas15.html', 'tablaspruebas.css', 'code.js', 'favicon.ico'];
+const cacheName = 'cache-v3.0.1.12';
+const precacheResources = ['/', 'index.html', 'Fechas15.html', 'Fechas15' ,'tablaspruebas.css', 'code.js', 'favicon.ico'];
 
 async function deleteCache(key) {
 	await caches.delete(key);
@@ -42,12 +42,13 @@ self.addEventListener('install', (event) => {
 				if (!response.ok) {
 					throw new TypeError("bad response status");
 				}
-				let CRV;
+/*				let CRV;
 				cleanResponse(response).then(CR => {
 					CRV = CR;
 				});
-				console.log(CRV)
-				cache.put(url, CRV);
+				console.log(CRV)*/
+				console.log(response)
+				cache.put(url, response);
 			});
 		})
 	}));
